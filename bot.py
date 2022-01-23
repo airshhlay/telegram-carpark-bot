27423d77773adbfcc05d7fc64a79534af098ee25
Ashleigh <ashleighaathar@gmail.com>
2022-01-24 00:43:48 +0800

    switch to webhook

--- bot.py
+++ bot.py
@@ -446,14 +446,14 @@ def main():
   # log all errors
   # dispatcher.add_error_handler(error)
 
-  # # Start the Bot
-  # updater.start_webhook(listen="0.0.0.0",
-  #                       port=int(PORT),
-  #                       url_path=TOKEN)
-  # updater.bot.setWebhook('https://noelle-carpark-bot.herokuapp.com/' + TOKEN)
-
   # Start the Bot
-  updater.start_polling()
+  updater.start_webhook(listen="0.0.0.0",
+                        port=int(PORT),
+                        url_path=TOKEN)
+  updater.bot.setWebhook('https://noelle-carpark-bot.herokuapp.com/' + TOKEN)
+
+  # # Start the Bot
+  # updater.start_polling()
   
   logger.info("Bot started ♥")
 
