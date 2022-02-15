62232537a63823d0bfd330ac05af89680bbc9671
Ashleigh <ashleighaathar@gmail.com>
2022-02-15 21:18:57 +0800

    use production polling

--- bot.py
+++ bot.py
@@ -647,13 +647,13 @@ def main():
   # dispatcher.add_error_handler(error)
 
   # Start the Bot (USE THIS FOR DEPLOYMENT)
-  # updater.start_webhook(listen="0.0.0.0",
-  #                       port=int(PORT),
-  #                       url_path=TOKEN)
-  # updater.bot.setWebhook('https://noelle-carpark-bot.herokuapp.com/' + TOKEN)
+  updater.start_webhook(listen="0.0.0.0",
+                        port=int(PORT),
+                        url_path=TOKEN)
+  updater.bot.setWebhook('https://noelle-carpark-bot.herokuapp.com/' + TOKEN)
 
   # Start the Bot (USE THIS IF RUNNING LOCALLY)
-  updater.start_polling()
+  # updater.start_polling()
   
   logger.info("Bot started ♥")
 
