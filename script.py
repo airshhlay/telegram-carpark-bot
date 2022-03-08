cbd1fea886344bcc6fbd21369fe217e9563d41a1
Ashleigh <ashleighaathar@gmail.com>
2022-03-08 17:51:32 +0800

    modify script to add coordinates

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import os
 import re
 
 import pymongo
+from svy21 import SVY21
 MONGO_CONNECTION_STR = "mongodb+srv://{mongoUser}:{mongoPwd}@cluster0.up9mp.mongodb.net/{databaseName}?retryWrites=true&w=majority"
 MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "wheretoparksg")
 MONGO_PWD = os.environ.get("MONGO_PWD", "C7vQCxgilPugEvyf")
@@ -23,5 +24,22 @@ data = {}
 with open('./carparks.json', 'r') as f:
   data = json.load(f)
 
+svy21 = SVY21()
 
-botCollection.update_one({"_id": carparkDataDoc['_id']}, {'$set': {"data": data}})
+checkPassed = True
+for item in data:
+  if "x_coord" not in item and "latitude" not in item:
+    print("FAILURE - missing coordinates for " + str(item))
+    checkPassed = False
+    continue
+  if "x_coord" not in item:
+    x, y = svy21.computeSVY21(item.get("latitude"), item.get("longitude"))
+    data["x_coord"], data["y_coord"] = x, y
+  if "latitude" not in item:
+    lat, lon = svy21.computeLatLon(item.get('x_coord'), item.get('y_coord'))
+    data["latitude"], data["longitude"] = lat, lon
+
+if checkPassed:
+  botCollection.update_one({"_id": carparkDataDoc['_id']}, {'$set': {"data": data}})
+  with open('./out.json', 'w') as f:
+    json.dump(data, f)
